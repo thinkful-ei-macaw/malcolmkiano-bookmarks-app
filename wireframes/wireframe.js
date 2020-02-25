d7371a2163fcc33a5dc70412d9a64af051730472
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-25 11:05:49 -0500

    Added navigation between wireframes for demo

--- wireframes/wireframe.js
+++ wireframes/wireframe.js
@@ -1,7 +1,15 @@
+/* global $ */
+
 function init() {
   $('#showCode').on('click', function(){
+    $('#wf-links').removeClass('active');
     $('#codeSection').toggleClass('active');
-  })
+  });
+
+  $('#showLinks').on('click', function(){
+    $('#codeSection').removeClass('active');
+    $('#wf-links').toggleClass('active');
+  });
 }
 
 $(init);
